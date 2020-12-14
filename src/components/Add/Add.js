178dbbc22df3bf8bf3d89ec6bd930859ad436258
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-12-14 10:27:30 +0100

    Add onClose props to add from List

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -1,14 +1,26 @@
-import './Add.scss'
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import "./Add.scss";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 class Add extends Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
-  render(){
-    return()
+  render() {
+    const { onClose } = this.props;
+    return (
+      <div className="modal">
+        <div className="modal-content">
+          <span className="close" onClick={onClose(false)}>
+            &times;
+          </span>
+          <h2> Crear nuevo Vídeo </h2>
+          AQUÍ VA NUESTRO EL FORMULARIO
+        </div>
+      </div>
+    );
   }
 }
 Add.propTypes = {
-}
-export default Add
+  onClose: PropTypes.func.isRequired,
+};
+export default Add;
