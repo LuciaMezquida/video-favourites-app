e77b6c00138b34785ee774e3ff0a84166408df4a
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-12-14 10:25:54 +0100

    Create Add component and onClick event in Header

---
+++ src/components/Add/Add.js
@@ -0,0 +1,14 @@
+import './Add.scss'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+class Add extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render(){
+    return()
+  }
+}
+Add.propTypes = {
+}
+export default Add
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
 import "./Header.scss";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-const Header = () => {
+const Header = ({ onClickAdd }) => {
   return (
     <header className="header">
       <h1 className="header__title">Mis vídeos favoritos</h1>
       <div className="header__container">
         <input
+          onClick={onClickAdd}
           title="Añadir nuevo vídeo"
           type="button"
           value="Añadir Video"
@@ -18,6 +20,10 @@ const Header = () => {
       </div>
     </header>
   );
+};
+
+Header.propTypes = {
+  onClickAdd: PropTypes.func.isRequired,
 };
 
 export default Header;
