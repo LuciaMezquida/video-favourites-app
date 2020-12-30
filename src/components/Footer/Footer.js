0734a59f984eb9cfaa6a062a904eea3f7af7bb25
Lucia Mezquida <lucia.mezquida@gmail.com>
2020-12-30 13:51:38 +0100

    Add social media in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,44 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer className="footer">
-      <small className="footer__text">Created by Lucía Mezquida</small>
+      <small className="footer__text">&copy; Lucía Mezquida</small>
+      <nav>
+        <ul className="footer__media">
+          <li>
+            <a
+              href="//github.com/LuciaMezquida"
+              target="_blank"
+              title="Github"
+              className="footer__media--github"
+              rel="noreferrer"
+            >
+              <i className="fab fa-github-alt"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="//www.linkedin.com/in/luciamezquidajuan/"
+              target="_blank"
+              title="Linkedin"
+              className="footer__media--linkedin"
+              rel="noreferrer"
+            >
+              <i className="fab fa-linkedin-in"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="//twitter.com/LuciCodes"
+              target="_blank"
+              title="Twitter"
+              className="footer__media--twitter"
+              rel="noreferrer"
+            >
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </footer>
   );
 };
